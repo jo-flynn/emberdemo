2db4e338b5a805825f653169c0aaa9e1867dff97
Joe Flynn <me@joeflynn.net>
2014-03-02 16:44:07 -0500

    basic template

---
+++ laravel/app/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <link rel="stylesheet" type="text/css" href="/res/bower_components/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="/res/css/app.css">
+
+    <body>
+        <h1>Todo MVC</h1>
+        @yield('content')
+    </body>
+
+    <script type="text/javascript" src="/res/bower_components/jquery/dist/jquery.js"></script>
+    <script type="text/javascript" src="/res/bower_components/bootstrap/dist/js/bootstrap.js"></script>
+</html>
